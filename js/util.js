773bc66e3d10349e3dcabca678e62abd0e627c87
Enya01 <lenochka111222999@gmail.com>
2023-05-21 23:35:12 +0500

    global fixx

--- js/util.js
+++ js/util.js
@@ -1,69 +1,66 @@
-const checkMaxSymbol = (str, maxLength) => str.length <= maxLength;
-
-function getRandomNumber(min, max) {
-  if (min < max && min >= 0 && max >= 0) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  }
-  if (min >= max) {
-    [min, max] = [max, min];
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  }
-  if (max < 0 || min < 0) {
-    throw new RangeError('Minimum and maximum values must be positive');
-  }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+//Функция getRandomPositiveInteger, возвращающая случайное целое число из переданного диапазона включительно
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
+//Функция, генерирующаю число,которое не повторяется из диапазона включительно
+function createRandom(min, max) {
+  const previousValues = [];
 
-const mixArray = (array) => (array.sort(() => Math.random() - 0.5));
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+//функция об ошибке отправки данных на сервер
+const ALERT_SHOW_TIME = 7000;
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = '#ff4e4e';
 
-const isEscapeKey = (evt) => (evt.key === 'Escape');
+  alertContainer.textContent = message;
 
-function debounce (callback, timeoutDelay = 500) {
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+// функция блока кнопки отправки данных
+const blockSubmitButton = (submitButton) => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Подождите';
+};
+
+const unblockSubmitButton = (submitButton) => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+function debounce(callback, timeoutDelay) {
   let timeoutId;
 
   return (...rest) => {
     clearTimeout(timeoutId);
+
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 }
-
-function getId () {
-  let lastGetId = 0;
-
-  return function () {
-    lastGetId += 1;
-    return lastGetId;
-  };
-}
-
-function throttle (callback, delayBetweenFrames = 500) {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-}
-
-function getRandomId (min, max) {
-  const earlyValues = [];
-
-  return function () {
-    let actualValue = getRandomNumber(min, max);
-    if (earlyValues.length >= (max - min + 1)) {
-      throw new Error ({'text': `Перебраны все числа из диапазона от ${min} до ${max}`});
-    }
-    while (earlyValues.includes(actualValue)) {
-      actualValue = getRandomNumber(min, max);
-    }
-    earlyValues.push(actualValue);
-    return actualValue;
-  };
-}
-
-export {getRandomNumber, checkMaxSymbol, mixArray, isEscapeKey, debounce, getId, getRandomId, throttle};
+export { getRandomPositiveInteger, createRandom,showAlert,blockSubmitButton,unblockSubmitButton,debounce};
